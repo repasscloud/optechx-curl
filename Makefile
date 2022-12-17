387eb954d4147502fdf5d0c2f15bb152c3a348ba
danijeljw-rpc <danijel@repasscloud.com>
2022-12-17 14:31:44 +1100

    update testing

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
-hellomake: src/followrel.c
+followrel: src/followrel.c
 	mkdir out
 	gcc -fdiagnostics-color=always -g src/followrel.c -o out/followrel -I.
+
+optxcurl: src/optxcurl.c
+	mkdir out
+	gcc -fdiagnostics-color=always -g src/optxcurl.c -o out/optxcur -I.
--- src/optxcurl.c
+++ src/optxcurl.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc != 4) {
+    if (argc != 2) {
         // Display help message if incorrect number of arguments
         printf("Usage: optxcurl <URL> <output_file_name> <output_path>\n");
         return 0;
@@ -12,10 +12,45 @@ int main(int argc, char *argv[])
 
     // Get URL, output file name, and output path from arguments
     char *url = argv[1];
-    char *output_file_name = argv[2];
-    char *output_path = argv[3];
+    //char *output_file_name = argv[2];
+    //char *output_path = argv[3];
 
-    // TODO: Add code to download file from URL and save it to the specified output path with the specified output file name
+    // Initialize CURL handle
+    CURL *curl = curl_easy_init();
+    if (!curl) {
+        fprintf(stderr, "Error: Failed to initialize CURL handle\n");
+        return 1;
+    }
+
+    // Set URL to follow
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+
+    // Set maximum number of redirects to follow
+    curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 10);
+
+    // Enable automatic redirection
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
+
+    // Perform the request
+    CURLcode res = curl_easy_perform(curl);
+    if (res != CURLE_OK) {
+        fprintf(stderr, "Error: Failed to follow URL: %s\n", curl_easy_strerror(res));
+        curl_easy_cleanup(curl);
+        return 1;
+    }
+
+    // Get final URL
+    char *final_url = NULL;
+    curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &final_url);
+    printf("Final URL: %s\n", final_url);
+
+    // Get HTTP status code of final URL
+    long http_code = 0;
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+    printf("HTTP status code of final URL: %ld\n", http_code);
+
+    // Clean up CURL handle
+    curl_easy_cleanup(curl);
 
     return 0;
 }

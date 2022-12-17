b28809581ec03d9f3ae6a9fc2fd938f31395aca3
danijeljw-rpc <danijel@repasscloud.com>
2022-12-17 14:35:34 +1100

    update testing

--- src/optxcurl.c
+++ src/optxcurl.c
@@ -2,61 +2,87 @@
 #include <stdlib.h>
 #include <curl/curl.h>
 
-// Callback function for writing data to a file
-size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
-    size_t written = fwrite(ptr, size, nmemb, stream);
-    return written;
-}
+// // Callback function for writing data to a file
+// size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+//     size_t written = fwrite(ptr, size, nmemb, stream);
+//     return written;
+// }
 
-int main(int argc, char *argv[])
-{
-    if (argc != 2) {
-        // Display help message if incorrect number of arguments
-        printf("Usage: optxcurl <URL> <output_file_name> <output_path>\n");
-        return 0;
+// int main(int argc, char *argv[])
+// {
+//     if (argc != 2) {
+//         // Display help message if incorrect number of arguments
+//         printf("Usage: optxcurl <URL> <output_file_name> <output_path>\n");
+//         return 0;
+//     }
+
+//     // Get URL, output file name, and output path from arguments
+//     char *url = argv[1];
+//     //char *output_file_name = argv[2];
+//     //char *output_path = argv[3];
+
+//     // Initialize CURL handle
+//     CURL *curl = curl_easy_init();
+//     if (!curl) {
+//         fprintf(stderr, "Error: Failed to initialize CURL handle\n");
+//         return 1;
+//     }
+
+//     // Set URL to follow
+//     curl_easy_setopt(curl, CURLOPT_URL, url);
+
+//     // Set maximum number of redirects to follow
+//     curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 10);
+
+//     // Enable automatic redirection
+//     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
+
+//     // Perform the request
+//     CURLcode res = curl_easy_perform(curl);
+//     if (res != CURLE_OK) {
+//         fprintf(stderr, "Error: Failed to follow URL: %s\n", curl_easy_strerror(res));
+//         curl_easy_cleanup(curl);
+//         return 1;
+//     }
+
+//     // Get final URL
+//     char *final_url = NULL;
+//     curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &final_url);
+//     printf("Final URL: %s\n", final_url);
+
+//     // Get HTTP status code of final URL
+//     long http_code = 0;
+//     curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+//     printf("HTTP status code of final URL: %ld\n", http_code);
+
+//     // Clean up CURL handle
+//     curl_easy_cleanup(curl);
+
+//     return 0;
+// }
+
+
+
+int main(void) {
+  CURL *curl;
+  CURLcode res;
+
+  curl = curl_easy_init();
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "http://example.com/file.txt");
+
+    // Set the output directory and file name
+    FILE *fp;
+    fp = fopen("/path/to/directory/file.txt", "wb");
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+
+    res = curl_easy_perform(curl);
+    if(res != CURLE_OK) {
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
     }
-
-    // Get URL, output file name, and output path from arguments
-    char *url = argv[1];
-    //char *output_file_name = argv[2];
-    //char *output_path = argv[3];
-
-    // Initialize CURL handle
-    CURL *curl = curl_easy_init();
-    if (!curl) {
-        fprintf(stderr, "Error: Failed to initialize CURL handle\n");
-        return 1;
-    }
-
-    // Set URL to follow
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-
-    // Set maximum number of redirects to follow
-    curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 10);
-
-    // Enable automatic redirection
-    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
-
-    // Perform the request
-    CURLcode res = curl_easy_perform(curl);
-    if (res != CURLE_OK) {
-        fprintf(stderr, "Error: Failed to follow URL: %s\n", curl_easy_strerror(res));
-        curl_easy_cleanup(curl);
-        return 1;
-    }
-
-    // Get final URL
-    char *final_url = NULL;
-    curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &final_url);
-    printf("Final URL: %s\n", final_url);
-
-    // Get HTTP status code of final URL
-    long http_code = 0;
-    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
-    printf("HTTP status code of final URL: %ld\n", http_code);
-
-    // Clean up CURL handle
     curl_easy_cleanup(curl);
-
-    return 0;
+    fclose(fp);
+  }
+  return 0;
 }
